7c5e2797a96cba3096137707cd722f4557fafeed
Laura Matic <lauramatic@gmail.com>
2018-03-28 16:04:10 +0200

    added request initial webpage read

--- start.py
+++ start.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 base_url = 'http://www.getjoan.com'
 r = requests.get(base_url)
-soup = BeautifulSoup(r.text)
+print(r)
+if r.status_code != 200:
+    print("Url not available")
+    sys.exit()
+print(r.text)
